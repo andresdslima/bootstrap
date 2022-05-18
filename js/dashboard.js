d2cc325c77584eec1fd4310fe9b5f25f930de130
Andre Lima <andresdslima@gmail.com>
2022-05-18 02:46:43 +0200

    refactoring register new product feature

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,16 +3,34 @@ const buttonRegister = document.querySelector('#btnRegister');
 const product = document.querySelector('#product');
 const price = document.querySelector('#price');
 const quantity = document.querySelector('#quantity');
+const tableBody = document.querySelector('#tableBody');
+
+let id = 4;
 
 buttonRegister.onclick = event => {
   event.preventDefault();
 
   if (product.value && price.value && quantity.value) {
+    tableBody.insertAdjacentHTML('beforeend', `     
+      <tr>
+        <th scope="row">${id}</th>
+        <td>${product.value}</td>
+        <td>${price.value}</td>
+        <td>${quantity.value}</td>
+        <td>
+          <button type="button" class="btn btn-danger">
+            <i class="fa-solid fa-trash-can btnDelete"></i>
+          </button>
+        </td>
+      </tr>
+    `);
+
     product.value = '';
     price.value = '';
     quantity.value = '';
 
     toastr.success('New product registered!');
+    return id++;
   };
 };
 
